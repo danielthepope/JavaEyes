ab212b8d4086abf6fa3d84ed6b9345dcef88924e
Daniel Pope <djpope2365@gmail.com>
2015-02-20 10:30:09 +0000

    Explains what the big number means
    Also displays the total RAM

--- JavaEyes/MainWindow.xaml.cs
+++ JavaEyes/MainWindow.xaml.cs
@@ -3,20 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace JavaEyes
@@ -35,6 +24,7 @@ namespace JavaEyes
             javas = new List<ProcessEye>();
             totalRam = new ComputerInfo().TotalPhysicalMemory;
             InitializeComponent();
+            AvailableRamLabel.Content = String.Format("Total RAM: {0:n0}MB", totalRam / 1048576.0);
             CountJava();
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += new EventHandler(CountJava);
@@ -50,14 +40,15 @@ namespace JavaEyes
             long ram = 0;
             foreach (Process p in processes)
             {
-                if (!p.ProcessName.Contains("JavaEyes") && p.ProcessName.ToLower().Contains(criteria))
+                if (!p.ProcessName.Contains("JavaEyes") && p.ProcessName.ToLower().Contains(criteria.ToLower()))
                 {
                     javas.Add(new ProcessEye(p));
                     ram += p.WorkingSet64;
                 }
             }
+            ProcessLabel.Content = javas.Count == 1 ? "process" : "processes";
             CountBox.Content = javas.Count;
-            Title = criteria + "Eyes";
+            SetTitle(criteria);
             RamLabel.Content = String.Format("{0:n0} MB RAM", ram / 1048576.0);
             ProcessList_Initialized(null, null);
             UpdateGraph(ram);
@@ -76,12 +67,28 @@ namespace JavaEyes
             CriteriaRam.Width = ram / bytesPerPixel;
         }
 
+        private void SetTitle(string criteria)
+        {
+            string title = "";
+            Regex filter = new Regex("[^A-z]");
+            criteria = filter.Replace(criteria, "");
+            if (criteria.Length == 0) title = "Eyes";
+            else if (criteria.Length == 1) title = criteria.ToUpper() + "Eyes";
+            else title = criteria.Substring(0, 1).ToUpper() + criteria.Substring(1).ToLower() + "Eyes";
+            Title = title;
+        }
+
         private void ProcessList_Initialized(object sender, EventArgs e)
         {
             ProcessList.ItemsSource = javas;
             var sortColumn = ProcessList.Columns[2];
             sortColumn.SortDirection = ListSortDirection.Descending;
             ProcessList.Items.SortDescriptions.Add(new SortDescription(sortColumn.SortMemberPath, ListSortDirection.Descending));
+        }
+
+        private void Criteria_KeyUp(object sender, KeyEventArgs e)
+        {
+            CountJava();
         }
     }
 }
